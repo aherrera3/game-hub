9410ebf2e1195ddb81b702ad56d2fd6cad9e7346
Angelica Herrera <angelicaherreraalba@gmail.com>
2023-05-02 10:34:25 -0500

    Show a spinner while fetching and fix missing key

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,13 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import { CanceledError } from "axios";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error.length > 0) return null; // [] its a truty value
+  if (isLoading) return <Spinner />;
 
   return (
     <List>
